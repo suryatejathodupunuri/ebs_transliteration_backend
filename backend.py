0579c92d69da2546a7aacee7697fe774489bb3a0
suryatejathodupunuri <165146287+suryatejathodupunuri@users.noreply.github.com>
2024-07-15 17:50:02 +0530

    added /test

--- backend.py
+++ backend.py
@@ -12,16 +12,19 @@ def transliterate():
             inp=data['inp']
             transliterate= Transliterator(source=src,target=tar,build_lookup=True)
             out= transliterate.transform(inp)
-            with open("input.txt", "w") as file:
+            """with open("input.txt", "a") as file:
                      file.write("Input:\n")
                      file.write(inp + "\n")
-            with open("output.txt", "w") as file:
+            with open("output.txt", "a") as file:
                     file.write("Output:\n")
-                    file.write(out+"\n")
+                    file.write(out+"\n")"""
             return jsonify({'output':out},{'status':'SUCCESS'}),200
         except KeyError:
             return jsonify({'error': 'Invalid JSON format or missing "text" field'}), 400
         except Exception as e:
             return jsonify({'error': str(e)}), 500
+@app.route('/test')
+def test():
+    return "Backend running successfully."
 if __name__ == '__main__':
     app.run(debug=True)
